ac9e9d2b8b1e6f5a917fc48ced336494d877ceaf
kaiok1337 <kaioklesson@gmail.com>
2022-12-21 14:30:04 -0700

    added useEffect to card api pulls

--- gameq/src/components/GameCard/index.js
+++ gameq/src/components/GameCard/index.js
@@ -1,24 +1,70 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './gameCard.css'
 
 const GameCard = () => {
-    const [currentGame, setCurrentGame] = useState({})
+    const [currentGame1, setCurrentGame1] = useState({})
+    const [currentGame2, setCurrentGame2] = useState({})
+    const [currentGame3, setCurrentGame3] = useState({})
+    const [currentGame4, setCurrentGame4] = useState({})
+
+    useEffect(() => {
+        getAllGames()
+    },[])
 
     async function getGameInfo(gameId) {
-        const { data } = await axios.get(`https://api.rawg.io/api/games/${gameId}?key=c97dea831b45422c91e825a94f59ee07`)
+        const { data } = await axios.get(`https://api.rawg.io/api/games/${gameId}?key=aa63c7887e7a4a0e804fe2a27c004822`)
         return data
     }
-    getGameInfo(25600)
-        .then(data => setCurrentGame(data))
+    
+    function getAllGames() {
+        getGameInfo('counter-strike-global-offensive')
+        .then(data => setCurrentGame1(data))
+        getGameInfo('fortnite')
+        .then(data => setCurrentGame2(data))
+        getGameInfo('call-of-duty-modern-warfare-ii')
+        .then(data => setCurrentGame3(data))
+        getGameInfo('escape-from-tarkov')
+        .then(data => setCurrentGame4(data))
+        
+    }
+    
+    
     return(
-        <div className="card" style={{width: "18rem"}}>
-            <img className="card-img-top" src={currentGame.background_image} alt="Card image cap" />
+        <main className="card-container">
+            <div className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src={currentGame1.background_image} alt="Card image cap" />
             <div className="card-body">
-                <h5 className="card-title">{currentGame.name}</h5>
-                <p className="card-text">{currentGame.rating}</p>
+                <h5 className="card-title">{currentGame1.name}</h5>
+                <p className="card-text">{currentGame1.rating}</p>
                 <a href="#" className="btn btn-primary">LIKE</a>
             </div>
-        </div>
+            </div>
+            <div className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src={currentGame2.background_image} alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{currentGame2.name}</h5>
+                <p className="card-text">{currentGame2.rating}</p>
+                <a href="#" className="btn btn-primary">LIKE</a>
+            </div>
+            </div>
+            <div className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src={currentGame3.background_image} alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{currentGame3.name}</h5>
+                <p className="card-text">{currentGame3.rating}</p>
+                <a href="#" className="btn btn-primary">LIKE</a>
+            </div>
+            </div>
+            <div className="card" style={{width: "18rem"}}>
+            <img className="card-img-top" src={currentGame4.background_image} alt="Card image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{currentGame4.name}</h5>
+                <p className="card-text">{currentGame4.rating}</p>
+                <a href="#" className="btn btn-primary">LIKE</a>
+            </div>
+            </div>
+        </main>
     )
 }
 
