d5b8d9698a684e0fe72171e2f626c5cceb3f0683
kaiok1337 <kaioklesson@gmail.com>
2022-12-21 15:00:16 -0700

    basic nav component started

--- gameq/src/App.js
+++ gameq/src/App.js
@@ -4,16 +4,22 @@ import GameCard from './components/GameCard'
 import './App.css';
 import SignUp from './pages/SignUp'
 import React, { useState, useEffect } from 'react';
+import Nav from './components/Nav'
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  console.log(isLoggedIn)
 
   return (
     <main>
+      <Nav />
       <h1>gameQ</h1>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home setIsLoggedIn={setIsLoggedIn} />}></Route>
         <Route path="/signup" element={<SignUp  />}></Route>
       </Routes>
+      {}
       <GameCard />
     </main>
   );
--- gameq/src/components/nav/index.js
+++ gameq/src/components/nav/index.js
@@ -1,45 +1,13 @@
-// packages
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-// styles
-import './styles.css'
+import { Routes, Route, Link } from 'react-router-dom'
 
-export default function Nav(props) {
-    // state declaration: build JSX array of NavBar items
-    const initialState = [
-        <div className='nav-item' key='1'>Theme</div>,
+    function Nav() {
+        return(
+            <nav>
+                <Link to='/' >Home</Link>
+                <Link to='/signup' >Sign Up</Link>
+                <Link to='/login' >Log In</Link>
+            </nav>
+        )
+    }
 
-        <div className='nav-item' key='2'>
-            <Link to='/'>
-                <img alt='blogr_logo' src='blogr_logo.png' />
-                <h1>logr</h1>
-            </Link>
-        </div>
-    ]
-    const [navItems, setNavItems] = useState(initialState)
-
-    // add NavBar items to JSX array depending on App login state
-    useEffect(() => {
-        if (props.isLoggedIn) {
-            setNavItems(initialState.concat(
-                <div className="nav-item" key='3'>
-                    <button onClick={() => { props.setLogInStatus(false) }}>Log Out</button>
-                </div>
-            ))
-        } else {
-            setNavItems(initialState.concat([
-                <div className="nav-item" key='3'>
-                    <button>Sign Up</button>
-                    <button>Log In</button>
-                </div>
-            ]))
-        }
-    }, [props.isLoggedIn])
-
-    // render JSX
-    return (
-        <nav>
-            {navItems}
-        </nav>
-    )
-}
+export default Nav
