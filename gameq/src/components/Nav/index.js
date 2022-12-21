420f91daae6bada90d14b3d9c086c6bd4b6a436d
Angel <angel.l.puente@gmail.com>
2022-12-21 16:54:57 -0600

    update nav

--- gameq/src/components/Nav/index.js
+++ gameq/src/components/Nav/index.js
@@ -1,11 +1,14 @@
 import { Routes, Route, Link } from 'react-router-dom'
-
+import './nav.css'
     function Nav() {
         return(
             <nav>
+            <button type="button" class="collapse"> Test</button>
+            <div class="content">
                 <Link to='/' >Home</Link>
                 <Link to='/signup' >Sign Up</Link>
                 <Link to='/login' >Log In</Link>
+            </div>
             </nav>
         )
     }
