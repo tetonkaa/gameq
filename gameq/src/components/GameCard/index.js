6fa9ba053ecab07be2d385ee69bf8b0a7f090a84
kaiok1337 <kaioklesson@gmail.com>
2022-12-21 17:36:17 -0700

    sign in and login forms

--- gameq/src/components/GameCard/index.js
+++ gameq/src/components/GameCard/index.js
@@ -32,7 +32,7 @@ const GameCard = () => {
     return(
         <main className="card-container">
             { currentGame.map((game, i) =>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card" key={i} style={{width: "18rem"}}>
             <img className="card-img-top" src={game.background_image} alt="Card image cap" />
             <div className="card-body">
                 <h5 className="card-title">{game.name}</h5>
