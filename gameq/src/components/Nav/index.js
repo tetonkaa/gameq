65ba5328a49d67bf56d4294fc0041e63345055d2
Angel <angel.l.puente@gmail.com>
2022-12-22 00:11:52 -0600

    styled closeable navbar

--- gameq/src/components/Nav/index.js
+++ gameq/src/components/Nav/index.js
@@ -1,15 +1,31 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import './nav.css'
-    function Nav() {
-        return(
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  
+  }
+
+
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  
+  } 
+function Nav() {
+  return(
             <nav>
-            <div class="content">
-                <Link to='/' >Home</Link>
-                <Link to='/signup' >Sign Up</Link>
-                <Link to='/login' >Log In</Link>
+            <div id="mySidebar" className="sidebar">
+            <a href="javascript:void(0)" className="closebtn" onClick={closeNav}> ></a>
+            <Link to='/' >Home</Link>
+            <Link to='/signup' >Sign Up</Link>
+            <Link to='/login' >Log In</Link>
             </div>
+
+        <div className="btncontainer">
+        <button className="openbtn" onClick={openNav}>GQ</button> 
+        </div> 
             </nav>
         )
-    }
-
-export default Nav
+      }
+      
+      export default Nav
