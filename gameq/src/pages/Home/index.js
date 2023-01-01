83e71d503689aceab02e486409c505d31e7eb7fe
Shavon63 <newgenesisnyc@gmail.com>
2023-01-01 07:29:00 -0500

    header added with 12 videos

--- gameq/src/pages/Home/index.js
+++ gameq/src/pages/Home/index.js
@@ -11,7 +11,61 @@ export default function Home(props) {
 
     // render JSX
     return (
-        <div>
+        <div className="video-gq">
+            <header>
+               {/* <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video.mp4" type="video/mp4"></source>
+                </video>  */}
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<div className="carosel" id="carosel1">
+  <a className="carosel-control carosel-control-left glyphicon glyphicon-chevron-left" href="#"></a>
+  <div className="carosel-inner">
+  <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video.mp4" type="video/mp4"></source>
+                </video> 
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video2.mp4" type="video/mp4"></source>
+                </video> 
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video3.mp4" type="video/mp4"></source>
+                </video> 
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video4.mp4" type="video/mp4"></source>
+                </video> 
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video5.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/video6.mp4" type="video/mp4"></source>
+                </video>
+  </div>
+  <a class="carosel-control carosel-control-right glyphicon glyphicon-chevron-right" href="#"></a>
+</div>
+<div className="carosel" id="carosel2">
+  <a className="carosel-control carosel-control-left glyphicon glyphicon-chevron-left" href="#"></a>
+  <div className="carosel-inner">
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset1.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset6.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset4.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset5.mp4" type="video/mp4"></source>
+                </video>
+                <video className="video-bg" autoPlay muted loop>
+                <source src="../../assets/video/videoset3.mp4" type="video/mp4"></source>
+                </video>
+  </div>
+  <a class="carosel-control carosel-control-right glyphicon glyphicon-chevron-right" href="#"></a>
+</div>
+            </header>
             <br />
         <main>
             <GameCard />
