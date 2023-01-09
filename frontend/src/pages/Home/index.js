ad1f78321eb018f3256fdfd535da7b4822a19aa5
Angel <angel.l.puente@gmail.com>
2023-01-08 19:19:25 -0600

    removed videos

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import './main.css';
 import GameCard from '../../components/GameCard'
-import VideoBroadcast from "../../components/VideoBroadcast";
+
 
 export default function Home(props) {
     
@@ -13,9 +13,7 @@ export default function Home(props) {
     // render JSX
     return (
         <div>        
-            <div className="video">
-                <VideoBroadcast/>
-            </div>
+            
             <main className="game-card">
                 <GameCard />
             </main>
